db932a94aaa3d034cbdf0411c09c507a1252b6fb
Mary <mlmartinez85@gmail.com>
2017-01-20 19:58:18 -0800

    string permutation checker

--- CodingQAs/chapter1.swift
+++ CodingQAs/chapter1.swift
@@ -12,9 +12,9 @@ import Foundation
 extension String {
     func hasUniqueChars() -> Bool {
         var lastChar: Character? = nil
-        let sorted = self.characters.sorted(by: <)  // time: O(slogs) space: O(s) where s is length of string
+        let sorted = self.characters.sorted()  // time: O(slogs) space: O(s) where s is length of string
         
-        for char in sorted {                        // time: O(s)
+        for char in sorted {                   // time: O(s)
             if let last = lastChar {
                 if last == char {
                     return false
@@ -24,4 +24,16 @@ extension String {
         }
         return true
     }
+}
+
+// 1.2 check if string is permutation of another string
+func isPermutation(textA: String, textB: String) -> Bool {
+    guard textA.characters.count == textB.characters.count else {
+        return false
+    }
+    
+    let sortedA = textA.lowercased().characters.sorted()    // time: O(slogs) space: O(s) where s is length of string
+    let sortedB = textB.lowercased().characters.sorted()    // time: O(slogs) space: O(s) where s is length of string
+    
+    return sortedA == sortedB
 }
--- CodingQAs/main.swift
+++ CodingQAs/main.swift
@@ -19,3 +19,15 @@ import Foundation
 print("\n1.1 Strig has Unique Characters \n\tTime: O(slogs) \n\tSpace: O(s) \n\twhere s is length of string")
 print("apple: \("apple".hasUniqueChars())")
 print("grape: \("grape".hasUniqueChars())")
+
+/*  1.2 Check if string is permutation of another
+	Time: O(slogs)
+	Space: O(s)
+	where s is length of string
+ 
+    apple vs grape: false
+    dog vs God: true
+*/
+print("\n1.2 Check if string is permutation of another \n\tTime: O(slogs) \n\tSpace: O(s) \n\twhere s is length of string")
+print("apple vs grape: \(isPermutation(textA: "apple", textB: "grape"))")
+print("dog vs God: \(isPermutation(textA: "dog", textB: "God"))")
